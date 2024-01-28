adaf88ca1c259a114d44118e5b40ce67455b891b
Teodor <teodorttodorov@icloud.com>
2024-01-28 22:15:41 +0200

    Closuers

--- Contents.swift
+++ Contents.swift
@@ -12,25 +12,40 @@
 //greeting2(whoToGreet: "Teodor")
 //
 
-struct Town {
-    let name: String
-    var citizens: [String]
-    var resources: [String: Int]
-    
-    init(townName: String, people: [String], stats: [String : Int]) {
-       name = townName
-       citizens = people
-       resources = stats
-    }
-    
-    func fortify() {
-        print("Defences increased!")
-    }
+//struct Town {
+//    let name: String
+//    var citizens: [String]
+//    var resources: [String: Int]
+//    
+//    init(townName: String, people: [String], stats: [String : Int]) {
+//       name = townName
+//       citizens = people
+//       resources = stats
+//    }
+//    
+//    func fortify() {
+//        print("Defences increased!")
+//    }
+//}
+//
+//var anotherTown = Town(townName: "Nameless Island", people: ["Sabrina Carpenter"], stats: ["Coconuts" : 100])
+//var ghostTown = Town(townName: "Ghosty McGhostface", people: [], stats: ["Tumbleweed" : 1])
+//
+//anotherTown.citizens.append("Me")
+//ghostTown.fortify()
+
+
+import UIKit
+
+func calculator (n1: Int, n2: Int, operation: (Int, Int) -> Int) -> Int {
+    return operation(n1,n2)
 }
 
-var anotherTown = Town(townName: "Nameless Island", people: ["Sabrina Carpenter"], stats: ["Coconuts" : 100])
-var ghostTown = Town(townName: "Ghosty McGhostface", people: [], stats: ["Tumbleweed" : 1])
+func add(no1: Int, no2: Int) -> Int {
+    return no1 + no2
+}
 
-anotherTown.citizens.append("Me")
-ghostTown.fortify()
+
+calculator(n1: 2, n2: 3, operation: add)
+
 
