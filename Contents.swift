db013835e3b37c8e8de09a27e4147094c934d1ec
Teodor <teodorttodorov@icloud.com>
2024-01-29 22:58:31 +0200

    Closure

--- Contents.swift
+++ Contents.swift
@@ -41,14 +41,9 @@ func calculator (n1: Int, n2: Int, operation: (Int, Int) -> Int) -> Int {
     return operation(n1,n2)
 }
 
-func add(no1: Int, no2: Int) -> Int {
-    return no1 + no2
-}
 
-func multiply(no1: Int, no2: Int) -> Int {
+calculator(n1: 2, n2: 3, operation: { (no1: Int, no2: Int) -> Int in
     return no1 * no2
-}
-
-calculator(n1: 2, n2: 3, operation: multiply)
-
+})
+ 
 
