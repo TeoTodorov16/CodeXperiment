9c39d739282574c64f4ab8f2a4247aa9bbbc1dab
Teodor <teodorttodorov@icloud.com>
2024-02-21 11:29:41 +0200

    Converting it into a Double

--- Contents.swift
+++ Contents.swift
@@ -55,20 +55,19 @@ import UIKit
 
 extension Double {
     
-    func round(to places: Int) {
-        let precisionNumber = pow(10, places)
+    func round(to places: Int) -> Double {
+        let precisionNumber = pow(10, Double(places))
         var n = self
         n = n * precisionNumber
+        n.round()
+        n = n / precisionNumber
+        return n
     }
 }
 
 var myDouble = 3.14159
 
-myDouble = myDouble * 1000
-myDouble.round()
-myDouble = myDouble / 1000
-
-myDouble.round(to:3)
+myDouble.round(to:1)
 
 
 
